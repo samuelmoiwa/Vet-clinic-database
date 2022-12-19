7e8fe4afeaa31d465041265c340db091af60f078
Moiwa <samuelmoiwa@gmail.com>
2022-12-19 02:40:06 -0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -42,3 +42,7 @@ CREATE TABLE visits(
 animal_id INT REFERENCES animals(id),
 vet_id INT REFERENCES vets(id),
 date_of_visit DATE);
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
